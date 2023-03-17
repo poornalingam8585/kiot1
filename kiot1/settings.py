f02856fb4d5ea12f5cef15df55194d50caef40b4
POORNALINGAM PATHMANABAN <lingamsiva8585@gmail.com>
2023-03-17 15:33:49 +0530

    message-susseccs

--- kiot1/settings.py
+++ kiot1/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-%+z37n-_9b1w!px_g-9e*0rr%*g6f8pn^dyheg!^i0777^inp$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
